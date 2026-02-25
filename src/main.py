56d230e200d2d80d287cec1daf36b89d297d189b
Aymanalaqrabawi <alqrabawiayman@gmail.com>
2026-02-26 00:01:26 +0300

    update main file

--- src/main.py
+++ src/main.py
@@ -5,12 +5,18 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 import pickle
+'''
+i load the model that i trained in the training phase,
+and i will use it to predict the class of the image
+that the user will upload to the api
+and the model is a keras model that i trained on the brain cancer dataset
+as will as the model is saved in a pickle file, and i will load it using the pickle library
 
-# تحميل النموذج
-with open("C:\\Users\\pc\\Downloads\\classifier_brain_cancer (1).pkl", "rb") as f:
+'''
+with open("classifier_brain_cancer.pkl", "rb") as f:
     brain_cancer = pickle.load(f)
 
-app = FastAPI(root_path="/api")
+app = FastAPI()
 
 router = APIRouter(
     prefix="/brain_cancer",
@@ -22,10 +28,9 @@ async def predict_brain_cancer(file: UploadFile):
     contents = await file.read()
     image = Image.open(BytesIO(contents)).convert("RGB")
     image = image.resize((224, 224))
-
-    # تحويل إلى numpy
+     # treanform the image to a numpy array and normalize it
     img_array = np.array(image)
-    # التنبؤ
+    img_array = img_array / 255.0
     preds = brain_cancer.predict(img_array)
     predicted_class = int(np.argmax(preds, axis=1)[0])
     if predicted_class == 0:
@@ -47,3 +52,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="localhost", port=8000)
+    # can be used for postman servers, or other servers that do not allow access to
